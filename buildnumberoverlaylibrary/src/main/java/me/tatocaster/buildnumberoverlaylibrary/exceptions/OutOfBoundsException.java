afb0a21537d9ace2837e4a7caad197a7e0f6532f
getsadzeg <getsadzeguram@gmail.com>
2017-02-10 23:35:07 +0400

    add dimen.xml, new Constants class and new Exception OutOfBoundsException

---
+++ buildnumberoverlaylibrary/src/main/java/me/tatocaster/buildnumberoverlaylibrary/exceptions/OutOfBoundsException.java
@@ -0,0 +1,13 @@
+package me.tatocaster.buildnumberoverlaylibrary.exceptions;
+
+
+
+public class OutOfBoundsException extends Exception {
+    public OutOfBoundsException() {
+        super();
+    }
+
+    public OutOfBoundsException(String msg) {
+        super(msg);
+    }
+}
---
+++ buildnumberoverlaylibrary/src/main/java/me/tatocaster/buildnumberoverlaylibrary/utils/Constants.java
@@ -0,0 +1,11 @@
+package me.tatocaster.buildnumberoverlaylibrary.utils;
+
+
+import android.graphics.Color;
+
+public final class Constants {
+    public static final int DEFAULT_BACKGROUND = Color.BLACK;
+    public static final int DEFAULT_TEXT = Color.RED;
+    public static final int DEFAULT_CANVAS_WIDTH = 300;
+    public static final int DEFAULT_CANVAS_HEIGHT = 150;
+}
